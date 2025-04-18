5ad3a810f0a0fcc57d38482cbb4c9194950b0463
Mert Bölükbaşı <mert54bolukbasi@gmail.com>
2025-04-18 12:21:55 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/' // GitHub Pages için repository adınızı base olarak ekliyoruz
+  base: '/Portfolio/' // GitHub repository adınız
 })
